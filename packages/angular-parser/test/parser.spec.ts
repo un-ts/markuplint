63786f77ce5379432a250c6efe61fa23708825f5
JounQin <admin@1stg.me>
2021-05-03 21:15:02 +0800

    feat: integration with angular-html-parser correctly (#7)

--- packages/angular-parser/test/parser.spec.ts
+++ packages/angular-parser/test/parser.spec.ts
@@ -1,7 +1,30 @@
-import { parse } from 'markuplint-angular-parser'
+import { cleanParse } from './helper'
 
 describe('parser', () => {
-  test('empty', () => {
-    expect(parse('')).toMatchSnapshot()
+  test('parse', () => {
+    expect(
+      cleanParse(/* HTML */ `<!DOCTYPE html>
+        <![CDATA[ Within this Character Data block I can use double dashes as
+        much as I want (along with <, &, ', and ") *and* %MyParamEntity; will be
+        expanded to the text "Has been expanded" ... however, I can't use the
+        CEND sequence. If I need to use CEND I must escape one of the brackets
+        or the greater-than sign using concatenated CDATA sections. ]]>
+        <!-- top level comment -->
+        <html lang="en">
+          <body>
+            <!-- body comment -->
+            <input [type]="'text'" />
+            <div>
+              Text1
+              <span>Text</span>
+              Text2
+            </div>
+          </body>
+        </html>`),
+    ).toMatchSnapshot()
+  })
+
+  test('invalid tag', () => {
+    expect(cleanParse('<a/></>')).toMatchSnapshot()
   })
 })

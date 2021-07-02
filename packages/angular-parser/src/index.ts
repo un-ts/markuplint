b63d67e9256da47cd70cae1b13898403b2846978
JounQin <admin@1stg.me>
2021-07-02 12:51:18 +0800

    fix(angular-parser): support namespaced svg tags like `svg:defs`

--- packages/angular-parser/src/index.ts
+++ packages/angular-parser/src/index.ts
@@ -135,7 +135,7 @@ const visitor = {
     namespace =
       attrs.find(attr => attr.name === 'xmlns')?.value ||
       namespace ||
-      (nodeName === 'svg'
+      (nodeName === 'svg' || nodeName.startsWith('svg:')
         ? 'http://www.w3.org/2000/svg'
         : 'http://www.w3.org/1999/xhtml')
 
